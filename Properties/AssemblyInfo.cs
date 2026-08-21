e34ab9de3c82224ea95269ad1b8ba4600c48dfc6
LerainzerGit <therecroommoviebackintime@gmail.com>
2026-08-21 23:19:41 +0100

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GameEngine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Athenos Games")]
+[assembly: AssemblyProduct("GameEngine")]
+[assembly: AssemblyCopyright("Copyright © Athenos Games")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
